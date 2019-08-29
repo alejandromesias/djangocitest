a564a6d37ce1f4da3fc94b155d3a3409d1d60941
alejandrom <amesias@thoughtworks.com>
2019-08-29 15:14:51 -0500

    heroku lybrary

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+import django_heroku
+django_heroku.settings(locals())
+
